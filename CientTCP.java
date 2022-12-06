7d4f5b196f22d3e9e483321c8d29503121ccbc27
rIccArDo20010 <104854215+rIccArDo20010@users.noreply.github.com>
2022-12-06 09:39:12 +0100

    primo client bozza

---
+++ CientTCP.java
@@ -0,0 +1,103 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cienttcp;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author lettie
+ */
+public class CientTCP {
+
+    /**
+     * @param args the command line arguments
+     */
+    Scanner scan;
+    Socket socket = null;
+    DataInputStream input = null;
+    DataOutputStream output = null;
+    InetAddress ip;
+   
+    public CientTCP() throws UnknownHostException, IOException{
+        ip = InetAddress.getByName("172.16.102.109");
+        socket = new Socket (ip,1234);
+        input = new DataInputStream(socket.getInputStream());
+        output = new DataOutputStream(socket.getOutputStream());
+        scan = new Scanner (System.in);
+    }
+   
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        CientTCP client = new CientTCP();
+        //thread 1->riceve i dati
+        client.readMessageThread();
+       
+       
+        //thread 2->invia i dati
+        client.writeMessageThread();
+    }
+   
+    private void readMessageThread()
+    {
+        Thread readmessage = new Thread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                while(true){
+                    String msg;
+                    try {
+                       
+                        msg = input.readUTF();
+                        log(msg); //metodo x visualizzare
+                    } catch (IOException ex) {
+                        Logger.getLogger(CientTCP.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                   
+                   
+                }
+            }
+        });
+        readmessage.start();
+    }
+   
+    private void writeMessageThread(){
+        Thread sendmessage = new Thread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                while(true){
+                    String msg = scan.nextLine();
+                    try {
+                        output.writeUTF(msg);
+                    } catch (IOException ex) {
+                        Logger.getLogger(CientTCP.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }
+        });
+        sendmessage.start();
+    }
+   
+   
+    private void log(String messaggio){
+       System.out.println(messaggio);
+    }
+
+}
